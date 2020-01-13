8d9665f1cc302b5337377b36d6fd0171b989a439
LeuAlmeida <leo@webid.net.br>
2020-01-13 14:19:45 -0300

    feat: Improving react hooks rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,8 @@ module.exports = {
       'warn',
       { extensions: ['.jsx', '.js'] }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
