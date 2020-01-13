3ba9f79760ffca425533952036e19d53793b5d83
LeuAlmeida <leo@webid.net.br>
2020-01-13 18:48:09 -0300

    chore: Improving app using react hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+
+  return (
+    <ul>
+      {tech.map(t => (
+        <li key={t}>{t}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
